d884d45ca67269cf9055170afc13b35139080f94
Pieter-Antonio <pieter.antonio.fernandez@gmail.com>
2023-04-20 09:45:45 +0200

    added multiply instruction

--- CA_Exercises/SIM/files_verilog.f
+++ CA_Exercises/SIM/files_verilog.f
@@ -1,15 +1,15 @@
-../Verilog/RTL/alu.v
-../Verilog/RTL/alu_control.v
-../Verilog/RTL/branch_unit.v
-../Verilog/RTL/control_unit.v
-../Verilog/RTL/cpu.v
-../Verilog/RTL/immediate_extend_unit.v
-../Verilog/RTL/mux_2.v
-../Verilog/RTL/pc.v
-../Verilog/RTL/reg_arstn.v
-../Verilog/RTL/reg_arstn_en.v
-../Verilog/RTL/register_file.v
-../Verilog/RTL/sram.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/alu.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/alu_control.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/branch_unit.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/control_unit.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/cpu.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/immediate_extend_unit.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/mux_2.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/pc.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/reg_arstn.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/reg_arstn_en.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/register_file.v
+../Verilog/RTL_SOLUTION2_multiplication_support_MULT2/sram.v
 ../Verilog/cpu_tb.v
 ../Verilog/sky130_sram_2rw.v
 
